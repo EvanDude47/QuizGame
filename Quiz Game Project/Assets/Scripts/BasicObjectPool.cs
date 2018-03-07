b9c55047ba5b0820e73f0c79735ffcbab17650a1
EvanDude47 <emthivierge@labs.neit.edu>
2018-03-07 13:31:33 -0500

    Quiz Game Setup

---
+++ Quiz Game Project/Assets/Scripts/BasicObjectPool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicObjectPool : MonoBehaviour
+{
+    public GameObject prefab;
+    private Stack<GameObject> inactiveInstances = new Stack<GameObject>();
+
+    //Take Object from pool
+	public GameObject GetObject()
+    {
+        GameObject spawnedObject;
+
+        if(inactiveInstances.Count > 0)
+        {
+            spawnedObject = inactiveInstances.Pop();
+        }
+        else
+        {
+            spawnedObject = Instantiate(prefab);
+
+            PooledObject pooledObject = spawnedObject.AddComponent<PooledObject>();
+            pooledObject.pool = this;
+            
+        }
+
+        spawnedObject.SetActive(true);
+
+        return spawnedObject;
+    }
+
+    //Return Object to pool
+    public void ReturnObject(GameObject toReturn)
+    {
+        PooledObject pooledObject = toReturn.GetComponent<PooledObject>();
+
+        if(pooledObject != null && pooledObject.pool == this)
+        {
+            toReturn.SetActive(true);
+            inactiveInstances.Push(toReturn);
+        }
+        else
+        {
+            Destroy(toReturn);
+        }
+    }
+}
+
+public class PooledObject : MonoBehaviour
+{
+    public BasicObjectPool pool;
+}
+
